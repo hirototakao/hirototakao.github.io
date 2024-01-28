21c58d6ffa7124bcd02c4fc021296825c75ccf1d
Hiroto Takao <132525711+hirototakao@users.noreply.github.com>
2024-01-28 22:03:06 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,15 +15,4 @@ let ball;
             y += (mouseY - y) * 0.04; 
             fill(0); 
             ellipse(x, y, 50, 50); 
-        } let ball;
-
-function setup() {
- createCanvas(windowWidth, windowHeight);
-}
-function draw() {
-   background(100);
-   let x += (mouseX - x) * 0.04; 
-   let y += (mouseY - y) * 0.04; 
-   fill(random(256), random(256), random(256)); 
-   ellipse(x, y, 50, 50);
-}
+        } 
