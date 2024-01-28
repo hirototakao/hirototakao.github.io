e1be3f99975c028d2e9d5151f1d94426764a8662
Hiroto Takao <132525711+hirototakao@users.noreply.github.com>
2024-01-28 22:20:36 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,6 +3,8 @@ let ball;
         let x = 0; 
         let y = 0; 
         createCanvas(windowWidth, windowHeight); 
+        canvas.style('z-index', 9999);
+        noCursor();
 
         // This method will draw the circle  
         // and track the mouse as well. 
