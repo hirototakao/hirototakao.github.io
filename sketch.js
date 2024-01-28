51798326b3078bd2c56058cd0c4218210b0e65c0
Hiroto Takao <132525711+hirototakao@users.noreply.github.com>
2024-01-28 22:43:18 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,7 @@
-let ball; 
-  
-        let x = 0; 
-        let y = 0; 
-        canvas.style('z-index', 9999);
-        noCursor();
+ //マウスストーカー用のdivを取得
+const stalker = document.getElementById('mouse-stalker'); 
 
-        // This method will draw the circle  
-        // and track the mouse as well. 
-        function draw() { 
-            x += (mouseX - x) * 0.1; 
-            y += (mouseY - y) * 0.1; 
-            fill(255); 
-            ellipse(x, y, 50, 50); 
-        } 
+//上記のdivタグをマウスに追従させる処理
+document.addEventListener('mousemove', function (e) {
+    stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
+});
