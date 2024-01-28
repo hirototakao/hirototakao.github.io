724d7c1c49ffdff56ff9e77bf011657606491126
Hiroto Takao <132525711+hirototakao@users.noreply.github.com>
2024-01-28 22:17:20 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,17 +2,14 @@ let ball;
   
         let x = 0; 
         let y = 0; 
-        // This method is used for creating canvas 
-        function setup() { 
-            createCanvas(windowWidth, windowHeight); 
-        } 
-  
+        createCanvas(windowWidth, windowHeight); 
+
         // This method will draw the circle  
         // and track the mouse as well. 
         function draw() { 
             background(255); 
-            x += (mouseX - x) * 0.04; 
-            y += (mouseY - y) * 0.04; 
+            x += (mouseX - x) * 0.1; 
+            y += (mouseY - y) * 0.1; 
             fill(0); 
             ellipse(x, y, 50, 50); 
         } 
