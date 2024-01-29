a8ffd82e856e504c7e71c544f30fc9fb57963b16
Hiroto Takao <132525711+hirototakao@users.noreply.github.com>
2024-01-29 09:06:21 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,14 @@
- //マウスストーカー用のdivを取得
+let x = 0;
+let y = 0;
+
+function draw() {
+	background(255);
+	x += (mouseX - x) * 0.2;
+	y += (mouseY - y) * 0.2;
+	fill(100);
+	noStroke();
+  circle(x, y, 50);
+} //マウスストーカー用のdivを取得
 const stalker = document.getElementById('mouse-stalker'); 
 
 //上記のdivタグをマウスに追従させる処理
