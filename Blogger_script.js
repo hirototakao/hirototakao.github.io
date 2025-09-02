0c1ae8245c01a0f6db4737e86cd9f3912ca62be7
hirototakao <billy0123210@outlook.com>
2025-09-02 10:21:48 +0900

    fix script execution error with Blogger_script.js

--- Blogger_script.js
+++ Blogger_script.js
@@ -1,22 +1,24 @@
-const searchBox = document.querySelector("#BlogSearch1 > div > form > div > input");
-const searchButton = document.querySelector("#BlogSearch1 > div > form > label > svg");
-      
-document.addEventListener("keydown", (event) => {
-  if(event.key === "Enter") {
+window.addEventListener('DOMContentLoaded', () => {
+  const searchBox = document.querySelector("#BlogSearch1 > div > form > div > input");
+  const searchButton = document.querySelector("#BlogSearch1 > div > form > label > svg");
+        
+  document.addEventListener("keydown", (event) => {
+    if(event.key === "Enter") {
+      if(!searchBox.value) {
+        event.preventDefault();
+      } else {
+        return;
+      }
+    } else {
+      return;
+    }
+  });
+          
+  searchButton.addEventListener("click", (event) => {
     if(!searchBox.value) {
       event.preventDefault();
     } else {
       return;
     }
-  } else {
-    return;
-  }
-});
-        
-searchButton.addEventListener("click", (event) => {
-  if(!searchBox.value) {
-    event.preventDefault();
-  } else {
-    return;
-  }
+  });
 });
