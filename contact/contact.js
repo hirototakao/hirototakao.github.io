a334102738d34fcc857ca9a816446fa2fb1785e8
hirototakao <billy0123210@outlook.com>
2025-09-10 11:00:38 +0900

    dfafasfsa

--- contact/contact.js
+++ contact/contact.js
@@ -1,9 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const twitterIcon = document.getElementById('twitterIcon');
   const githubIcon = document.getElementById('githubIcon');
-  twitterIcon.setAttribute('height', '25');
-  twitterIcon.setAttribute('width', '30');
-  
   githubIcon.setAttribute('height', '27');
   githubIcon.setAttribute('width', '30');
 });
@@ -20,11 +16,6 @@ function closePopUp() {
   document.getElementById('navbar').setAttribute("style", "background-color:rgb(248, 249, 250)!important;");
 }
 
-function clipboard(id) {
-  　const email = document.getElementById(id).innerText;
-    navigator.clipboard.writeText(email);
-    swal("Copied!", "You copied it to clipboard", "success")
-}
 function clipboardForTextBox(id) {
     const InputValue = document.getElementById(id).value;
     navigator.clipboard.writeText(InputValue);
